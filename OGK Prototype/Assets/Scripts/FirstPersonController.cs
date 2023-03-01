c9a856a233e1a6043f582e1276e8c08e400762bf
SamHenderson-1 <71032897+SamHenderson-1@users.noreply.github.com>
2023-03-01 00:55:28 -0700

    Pre-Alpha Build Ready

--- OGK Prototype/Assets/Scripts/FirstPersonController.cs
+++ OGK Prototype/Assets/Scripts/FirstPersonController.cs
@@ -123,6 +123,7 @@ namespace StarterAssets
 			_input = GetComponent<StarterAssetsInputs>();
 #if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
 			_playerInput = GetComponent<PlayerInput>();
+			playerUI = GetComponent<PlayerUI>();
 #else
 			Debug.LogError( "Starter Assets package is missing dependencies. Please use Tools/Starter Assets/Reinstall Dependencies to fix it");
 #endif
