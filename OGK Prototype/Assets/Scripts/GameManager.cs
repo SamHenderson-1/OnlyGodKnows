26891712bd4891b7b35e52aae9c5003b0943617d
SamHenderson-1 <71032897+SamHenderson-1@users.noreply.github.com>
2023-03-29 00:19:02 -0600

    Game loop implemented

--- OGK Prototype/Assets/Scripts/GameManager.cs
+++ OGK Prototype/Assets/Scripts/GameManager.cs
@@ -1,11 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     bool gameHasEnded = false;
+    private void Awake()
+    {
+        UnityEngine.SceneManagement.Scene currentScene = SceneManager.GetActiveScene();
 
+        // Retrieve the name of this scene.
+        string sceneName = currentScene.name;
+        if (sceneName == "MainMenu") {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+    }
     public void EndGame() {
         if (!gameHasEnded) { 
             gameHasEnded = true;
