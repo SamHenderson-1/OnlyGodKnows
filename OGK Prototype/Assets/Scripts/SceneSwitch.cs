bc5f9da1a475e72142fbf1009e96b3e982a86621
SamHenderson-1 <71032897+SamHenderson-1@users.noreply.github.com>
2023-04-23 16:11:58 -0600

    Added notes, some SFX, and started work on dialogue.

--- OGK Prototype/Assets/Scripts/SceneSwitch.cs
+++ OGK Prototype/Assets/Scripts/SceneSwitch.cs
@@ -1,13 +1,17 @@
+using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using NoteSystem;
 
 public class SceneSwitch : MonoBehaviour
 {
     public int sceneToSwitchTo;
+    public FirstPersonController player;
     void OnTriggerEnter(Collider other)
     {
+        player.startingText = player.startingNote.Pages[0].text;
         SceneManager.LoadScene(sceneToSwitchTo);
     }
 }
