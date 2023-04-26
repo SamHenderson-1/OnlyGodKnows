ffc2130ccc9010d87cfde2806f3e75997c403b1e
SamHenderson-1 <71032897+SamHenderson-1@users.noreply.github.com>
2023-04-25 22:56:24 -0600

    Edited scene transitions and cutscenes

--- OGK Prototype/Assets/Scripts/UI/MainMenu/Main_Menu.cs
+++ OGK Prototype/Assets/Scripts/UI/MainMenu/Main_Menu.cs
@@ -1,12 +1,31 @@
 
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class Main_Menu : MonoBehaviour
 {
+    [SerializeField]
+    VideoPlayer player;
+    [SerializeField]
+    RawImage cutscene;
+
+    public void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
     public void StartGame()
     {
         Debug.Log("Loading Playground scene");
+        cutscene.gameObject.SetActive(true);
+        player.Play();
+        player.loopPointReached += EndReached;
+            
+    }
+
+    void EndReached(VideoPlayer vp)
+    {
         SceneManager.LoadScene("Midwich");
     }
 
